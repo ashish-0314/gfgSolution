93fd2142327adc21c4c45048fc4276cf59bcfc81
Ashish Kumar <149486193+ashish-0314@users.noreply.github.com>
2026-05-19 13:40:55 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Shortest path in Directed Acyclic Graph/shortest-path-in-directed-acyclic-graph.cpp
@@ -0,0 +1,47 @@
+// User function Template for C++
+class Solution {
+  public:
+  
+    void topoSort(int node ,vector<vector<pair<int,int>>> &adj , vector<int> & vis , stack<int> & st){
+        vis[node]=1;
+        for(auto & it : adj[node]){
+            int v = it.first;
+            if(!vis[v]) topoSort(v,adj,vis,st);
+        }
+        st.push(node);
+    }
+    vector<int> shortestPath(int V, int E, vector<vector<int>>& edges) {
+        // code here
+        vector<vector<pair<int,int>>> adj(V) ;
+        for(auto & e : edges){
+            int u = e[0];
+            int v = e[1];
+            int wt = e[2];
+            adj[u].push_back({v,wt});
+        }
+        vector<int> vis(V,0);
+        stack<int> st ;
+        for(int i = 0 ; i < V ; i++){
+            if(!vis[i]) topoSort(i,adj,vis,st);
+        }
+        vector<int> dist(V,1e9);
+        
+        dist[0]=0;
+        while(!st.empty()){
+            int node = st.top();
+            st.pop();
+            if(dist[node] != 1e9){
+                for(auto it : adj[node]){
+                    int v = it.first;
+                    int wt = it.second;
+                    if(dist[node]+wt < dist[v]) dist[v]= dist[node]+wt;
+                }
+            }
+        }
+        for(int i = 0 ; i < V ; i++){
+            if(dist[i]==1e9) dist[i]=-1;
+        }
+        return dist;
+        
+    }
+};
